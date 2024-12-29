e50e99fa87a076f0726bf90b5259244f2275202c
Suresh Pal <palsuresh8158@gmail.com>
2024-12-29 16:13:34 +0530

    update cors problem

--- backend/index.ts
+++ backend/index.ts
@@ -10,12 +10,7 @@ const port = process.env.PORT || 8000
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(
-     cors({
-          origin: "http://localhost:5173",
-          credentials: true,
-     })
-);
+app.use(cors());
 
 // routes
 import authRoute from "./routes/authRoute"
